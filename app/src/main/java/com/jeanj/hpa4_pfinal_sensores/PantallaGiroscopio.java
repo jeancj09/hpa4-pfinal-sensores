989466f416240d527add56e475b2d530c85c727d
RoiHatsune7 <janielpinzon@yahoo.es>
2022-12-10 23:34:06 -0500

    Correccion a pantalla de giroscopio y su codigo

--- app/src/main/java/com/jeanj/hpa4_pfinal_sensores/PantallaGiroscopio.java
+++ app/src/main/java/com/jeanj/hpa4_pfinal_sensores/PantallaGiroscopio.java
@@ -36,7 +36,7 @@ public class PantallaGiroscopio extends AppCompatActivity implements SensorEvent
         buttongyro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(this, MainActivity.class);
+                Intent intent = new Intent(PantallaGiroscopio.this, MainActivity.class);
 
             }
         });
