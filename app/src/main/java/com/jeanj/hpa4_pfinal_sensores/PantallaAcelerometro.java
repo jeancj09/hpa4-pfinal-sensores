ae2690a03d86860ad6daaeee2ac186e10a42ff0c
jeancj09 <jeanjez92@gmail.com>
2022-12-11 00:17:27 -0500

    Agregado el diseño y código, pantalla acelerometro.

--- app/src/main/java/com/jeanj/hpa4_pfinal_sensores/PantallaAcelerometro.java
+++ app/src/main/java/com/jeanj/hpa4_pfinal_sensores/PantallaAcelerometro.java
@@ -2,13 +2,125 @@ package com.jeanj.hpa4_pfinal_sensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class PantallaAcelerometro extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PantallaAcelerometro extends AppCompatActivity implements SensorEventListener {
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+    Button bttSalirPA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_acelerometro);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        bttSalirPA = findViewById(R.id.bttSalirPA);
+
+        bttSalirPA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mysensor = event.sensor;
+
+        if (mysensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+                if (speed > SHAKE_THRESHOLD) {
+                    getRandomNumber();
+
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void getRandomNumber() {
+        ArrayList numbersGenerated = new ArrayList();
+
+        for (int i = 0; i < 6; i++) {
+            Random randNumber = new Random();
+            int iNumber = randNumber.nextInt(99) + 1;
+
+            if(!numbersGenerated.contains(iNumber)) {
+                numbersGenerated.add(iNumber);
+            } else {
+                i--;
+            }
+        }
+
+        TextView tvnum01, tvnum02, tvnum03, tvnum04, tvnum05, tvnum06;
+        tvnum01 = findViewById(R.id.tvnum1);
+        tvnum02 = findViewById(R.id.tvnum2);
+        tvnum03 = findViewById(R.id.tvnum3);
+        tvnum04 = findViewById(R.id.tvnum4);
+        tvnum05 = findViewById(R.id.tvnum5);
+        tvnum06 = findViewById(R.id.tvnum6);
+
+        tvnum01.setText(""+numbersGenerated.get(0));
+        tvnum02.setText(""+numbersGenerated.get(1));
+        tvnum03.setText(""+numbersGenerated.get(2));
+        tvnum04.setText(""+numbersGenerated.get(3));
+        tvnum05.setText(""+numbersGenerated.get(4));
+        tvnum06.setText(""+numbersGenerated.get(5));
+
+
     }
 }
